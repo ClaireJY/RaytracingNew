062ba91b4e1153342bec9a5105c461648b26d581
clairejy <junyi.wang@yo-star.com>
2020-10-29 19:35:56 +0800

    sync

--- RaytracingNew/InOneWeekend/main.cc
+++ RaytracingNew/InOneWeekend/main.cc
@@ -48,7 +48,7 @@ hittable_list random_scene() {
 
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
-
+    
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
@@ -76,7 +76,7 @@ hittable_list random_scene() {
             }
         }
     }
-
+    
     auto material1 = make_shared<dielectric>(1.5);
     world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
@@ -95,7 +95,7 @@ int main() {
     // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 800;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 10;
     const int max_depth = 50;
@@ -106,10 +106,10 @@ int main() {
 
     // Camera
 
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
+    point3 lookfrom(15,8,13);
+    point3 lookat(0,-2,0);
     vec3 vup(0,1,0);
-    auto dist_to_focus = 10.0;
+    auto dist_to_focus = 10.0;//æ∞…Ó
     auto aperture = 0.1;
 
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
@@ -125,6 +125,8 @@ int main() {
             for (int s = 0; s < samples_per_pixel; ++s) {
                 auto u = (i + random_double()) / (image_width-1);
                 auto v = (j + random_double()) / (image_height-1);
+				//auto u = ((double)i ) / (image_width-1);
+			    //auto v = ((double)j) / (image_height-1);
                 ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world, max_depth);
             }
